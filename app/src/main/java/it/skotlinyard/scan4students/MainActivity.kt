f9f6635dd262e6a37eaa6333adf425253aae0a5d
kecco9939 <kecco9939@gmail.com>
2021-04-29 11:31:59 +0200

    Initial commit

---
+++ app/src/main/java/it/skotlinyard/scan4students/MainActivity.kt
@@ -0,0 +1,11 @@
+package it.skotlinyard.scan4students
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
